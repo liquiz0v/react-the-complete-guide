f83c257d6e5cd6ce83f29ad3c5cd8c978ddc7b48
Vladislav <vladislav.sheyko@hotmail.com>
2021-01-24 22:39:27 +0200

    Finished module 8 (lecure 158)

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(ingredient =>
-            <li key={ingredient}>
-                <span style={{TextTransform: 'capitalize'}}>{ingredient}</span>: {props.ingredients[ingredient]}
-            </li>);
-    return (<Aux>
-        <h3>Your order</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>
-            {ingredientSummary}
-        </ul>
-        <p>Total price is: <strong>{props.totalPrice.toFixed(2)}</strong></p>
-        <p>Continue to checkout?</p>
-        <Button btnType="Success" clicked={props.onContinueClick}>CONTINUE</Button>
-        <Button btnType="Danger" clicked={props.onCancelClick}>CLOSE</Button>
-    </Aux>);
-};
+import React, {Component} from 'react';
 
-export default orderSummary;
+class OrderSummary extends Component {
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("[OrderSummary] componentDidUpdate")
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(ingredient =>
+                <li key={ingredient}>
+                    <span style={{TextTransform: 'capitalize'}}>{ingredient}</span>: {this.props.ingredients[ingredient]}
+                </li>);
+        return (<Aux>
+            <h3>Your order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Total price is: <strong>{this.props.totalPrice.toFixed(2)}</strong></p>
+            <p>Continue to checkout?</p>
+            <Button btnType="Success" clicked={this.props.onContinueClick}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.onCancelClick}>CLOSE</Button>
+        </Aux>);
+    }
+}
+
+export default OrderSummary;
